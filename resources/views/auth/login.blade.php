19f3972a814a64c09297b144d9509598e84a48e7
nawan <ekosetiyawan@hotmail.com>
2024-03-16 14:41:43 +0700

    fixing login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,8 +49,8 @@
     }  
 </style>
 <!-- Font Awesome -->
-<link rel="stylesheet" href="assets/fontawesome-free/css/all.min.css">
-<link rel="stylesheet" href="css/bg.css">
+<link rel="stylesheet" href="/assets/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="/css/bg.css">
 {{-- bootstrap@5.3.0-alpha3 css --}}
 <link rel="stylesheet" href="{{ URL::asset('/assets/bootstrap-5/css/bootstrap.min.css') }}" />
 <body class="bg-login">
