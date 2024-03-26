ce23f1c3f557b4b184e764fdac0aa2710423c490
nawan <ekosetiyawan@hotmail.com>
2024-03-26 15:39:30 +0700

    fixing api link image url

--- app/Http/Controllers/api/BahanController.php
+++ app/Http/Controllers/api/BahanController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Tool;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 use Symfony\Component\HttpFoundation\Response;
 
 class BahanController extends Controller
@@ -17,7 +18,31 @@ class BahanController extends Controller
     {
         try {
             $dataBahan = Tool::all();
-            return response()->json($dataBahan, Response::HTTP_OK);
+            // $dataBahan = Tool::select('id', 'name', 'jml_stok', 'foto_bahan')->get('foto_bahan');
+            // return $this->sendResponse(
+            //     [
+            //         'id' => $dataBahan->id,
+            //         'name' => $dataBahan->name,
+            //         'kode_bahan' => $dataBahan->kode_bahan,
+            //         'status_bahan' => $dataBahan->status_bahan,
+            //         'jml_stok' => $dataBahan->jml_stok,
+            //         'price' => $dataBahan->price,
+            //         'foto_bahan' => URL::to('/') . 'bahan/' . $dataBahan->foto_bahan,
+            //         'note' => $dataBahan->note,
+            //         'deskripsi' => $dataBahan->deskripsi,
+            //         'created_at' => $dataBahan->created_at,
+            //         'updated_at' => $dataBahan->updated_at,
+            //     ],
+            //     'Data Bahan Berhasil Ditampilkan'
+            // );
+            //return response()->json($dataBahan, Response::HTTP_OK);
+            $tampil = [];
+
+            foreach ($dataBahan as $data) {
+                $data->foto_bahan = env('APP_URL') . '/' . 'storage/' . $data->foto_bahan;
+                array_push($tampil, $data);
+            }
+            return response()->json($tampil);
         } catch (QueryException $e) {
             $error = [
                 'error' => $e->getMessage()
