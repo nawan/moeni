b41a26afc8ad3cc7cbfe6eef776400ce3f1b932f
nawan <ekosetiyawan@hotmail.com>
2024-04-16 14:23:35 +0700

    update api bahan

--- app/Http/Controllers/api/BahanController.php
+++ app/Http/Controllers/api/BahanController.php
@@ -7,6 +7,7 @@ use App\Models\Tool;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class BahanController extends Controller
@@ -18,24 +19,6 @@ class BahanController extends Controller
     {
         try {
             $dataBahan = Tool::all();
-            // $dataBahan = Tool::select('id', 'name', 'jml_stok', 'foto_bahan')->get('foto_bahan');
-            // return $this->sendResponse(
-            //     [
-            //         'id' => $dataBahan->id,
-            //         'name' => $dataBahan->name,
-            //         'kode_bahan' => $dataBahan->kode_bahan,
-            //         'status_bahan' => $dataBahan->status_bahan,
-            //         'jml_stok' => $dataBahan->jml_stok,
-            //         'price' => $dataBahan->price,
-            //         'foto_bahan' => URL::to('/') . 'bahan/' . $dataBahan->foto_bahan,
-            //         'note' => $dataBahan->note,
-            //         'deskripsi' => $dataBahan->deskripsi,
-            //         'created_at' => $dataBahan->created_at,
-            //         'updated_at' => $dataBahan->updated_at,
-            //     ],
-            //     'Data Bahan Berhasil Ditampilkan'
-            // );
-            //return response()->json($dataBahan, Response::HTTP_OK);
             $tampil = [];
 
             foreach ($dataBahan as $data) {
@@ -56,7 +39,25 @@ class BahanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required'
+            ]);
+            if ($validator->fails()) {
+                return response()->json(
+                    ['error' => $validator->errors()],
+                    Response::HTTP_UNPROCESSABLE_ENTITY
+                );
+            }
+            Tool::create($request->all());
+            $response = ['Success' => 'Bahan berhasil terinput'];
+            return response()->json($response, Response::HTTP_CREATED);
+        } catch (QueryException $e) {
+            $error = [
+                'error' => $e->getMessage()
+            ];
+            return response()->json($error, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
     }
 
     /**
@@ -64,6 +65,24 @@ class BahanController extends Controller
      */
     public function show(string $id)
     {
+        try {
+            $dataBahan = Tool::findOrFail($id);
+            $tampil = [
+                'name' => $dataBahan->name,
+                'kode_bahan' => $dataBahan->kode_bahan,
+                'status_bahan' => $dataBahan->status_bahan,
+                'jml_stok' => $dataBahan->jml_stok,
+                'price' => $dataBahan->price,
+                'foto_bahan' => env('APP_URL') . '/' . 'public/storage/' . $dataBahan->foto_bahan,
+                'note' => $dataBahan->note,
+                'deskripsi' => $dataBahan->deskripsi
+            ];
+            return response()->json($tampil, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Hasil Tidak Ditemukan'
+            ], Response::HTTP_FORBIDDEN);
+        }
     }
 
     /**
@@ -71,7 +90,27 @@ class BahanController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $dataBahan = Tool::findOrFail($id);
+            $validator = Validator::make($request->all(), [
+                'name' => 'required'
+            ]);
+            if ($validator->fails()) {
+                return response()->json(
+                    ['succeed' => false, 'Message' => $validator->errors()],
+                    Response::HTTP_UNPROCESSABLE_ENTITY
+                );
+            }
+            $dataBahan->update($request->all());
+            $response = [
+                'Success' => 'Data Bahan Berhasil Diperbarui'
+            ];
+            return response()->json($response, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Gagal Update'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
     }
 
     /**
@@ -79,6 +118,13 @@ class BahanController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            Tool::findOrFail($id)->delete();
+            return response()->json(['success' => 'Data Bahan Berhasil Dihapus']);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Gagal Hapus'
+            ], Response::HTTP_FORBIDDEN);
+        }
     }
 }
